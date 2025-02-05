695311132a62ccfa4db19920d199fd8947675024
DRAMACEBEPA <dramamailnet@gmail.com>
2025-02-05 20:23:31 +0300

    Fixed capture interface

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from tensorflow.keras.layers import Dense
 
 # === Параметры ===
 TRAINING_PERIOD = 0.3 * 60 * 60  # Период сбора обучающей выборки (24 часа)
-CAPTURE_INTERFACE = "enp92s0"  # Сетевой интерфейс для захвата трафика
+CAPTURE_INTERFACE = "eth0"  # Сетевой интерфейс для захвата трафика
 MODEL_PATH = "anomaly_detection_model.h5"  # Путь для сохранения/загрузки модели
 SPECIAL_SYMBOLS = "!@#$%^&*()-+"
 
